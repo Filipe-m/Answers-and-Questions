c205c7ce451f662258a5b29d6dbb6a626efe683e
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2023-03-21 11:55:21 -0300

    Public folder and bootstrap config

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express'
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render("index")
+  res.render('index')
 })
 
 app.listen(3333, () => {
